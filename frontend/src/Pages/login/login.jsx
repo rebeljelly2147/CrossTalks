739412e9f97bfa2469e0aa7199de85c5ce3cf480
rebeljelly2147 <c4coder3377@gmail.com>
2025-01-21 19:25:23 +0530

    Enhance login page UI and update background image

--- frontend/src/Pages/login/login.jsx
+++ frontend/src/Pages/login/login.jsx
@@ -1,5 +1,93 @@
 export const Login = () => {
   return (
-    <div>Login</div>
+    <div className="flex flex-col items-center justify-center min-w-96 mx-auto">
+      <div className="w-full p-6 rounded-lg shadow-md bg-gray-900 bg-clip-padding backdrop-blur-sm bg-opacity-30">
+        <h1 className="text-3xl font-semibold text-center text-gray-300">
+          Login 
+          <span className="text-pink-900"> ChatLiii</span>
+        </h1>
+
+        <form action="">
+          <div className="mb-4">
+            <label htmlFor="password" className="label p-2">
+              <span className="text-base label-text"> Username </span>
+            </label>
+            <input
+              type="text"
+              id="username"
+              className="w-full input input-bordered h-10 px-4 py-2 rounded-md bg-gray-800 text-gray-300"
+              placeholder="Enter your username"
+            />
+
+            <div>
+              <label htmlFor="password" className="label p-2">
+                <span className="text-base label-text"> Password</span>
+              </label>
+              <input 
+                type="text"
+                className="w-full input input-bordered h-10 px-4 py-2 rounded-md bg-gray-800 text-gray-300"
+                placeholder="Enter your password"
+              />
+            </div>
+            <a href="#" className="text-pink-900 text-sm hover:underline hover:text-pink-800 mt-2 inline-block">
+              {"Don't "}have an account? Sign Up
+              {/* here {"Don't "} is due to the space in between */}
+            </a>
+          </div>
+
+          <div>
+          <button className="btn btn-block btn-sm bg-pink-900 hover:bg-pink-800 text-white sm:btn-sm md:btn-md lg:btn-lg mt-2">Login</button>
+          </div>
+        </form>
+      </div>
+    </div>
   )
 }
+
+
+// Starter Code for all the File
+// export const Login = () => {
+//   return (
+//     <div className="flex flex-col items-center justify-center min-w-96 mx-auto">
+//       <div className="w-full p-6 rounded-lg shadow-md bg-gray-900 bg-clip-padding backdrop-blur-sm bg-opacity-30">
+//         <h1 className="text-3xl font-semibold text-center text-gray-300">
+//           Login 
+//           <span className="text-pink-900"> ChatLiii</span>
+//         </h1>
+
+//         <form action="">
+//           <div className="mb-4">
+//             <label htmlFor="password" className="label p-2">
+//               <span className="text-base label-text"> Username </span>
+//             </label>
+//             <input
+//               type="text"
+//               id="username"
+//               className="w-full input input-bordered h-10 px-4 py-2 rounded-md bg-gray-800 text-gray-300"
+//               placeholder="Enter your username"
+//             />
+
+//             <div>
+//               <label htmlFor="password" className="label p-2">
+//                 <span className="text-base label-text"> Password</span>
+//               </label>
+//               <input 
+//                 type="text"
+//                 className="w-full input input-bordered h-10 px-4 py-2 rounded-md bg-gray-800 text-gray-300"
+//                 placeholder="Enter your password"
+//               />
+//             </div>
+//             <a href="#" className="text-pink-900 text-sm hover:underline hover:text-pink-800 mt-2 inline-block">
+//               {"Don't "}have an account? Sign Up
+//               {/* here {"Don't "} is due to the space in between */}
+//             </a>
+//           </div>
+
+//           <div>
+//           <button className="btn btn-block btn-sm bg-pink-900 hover:bg-pink-800 text-white sm:btn-sm md:btn-md lg:btn-lg mt-2">Login</button>
+//           </div>
+//         </form>
+//       </div>
+//     </div>
+//   )
+// }
