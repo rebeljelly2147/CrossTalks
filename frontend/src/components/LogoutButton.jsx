0b86420cec42736cbb81d8605a5d025ca45e90d3
rebeljelly2147 <c4coder3377@gmail.com>
2025-01-22 16:42:43 +0530

    Add cursor pointer to LogoutButton icon for better UX

--- frontend/src/components/LogoutButton.jsx
+++ frontend/src/components/LogoutButton.jsx
@@ -2,7 +2,7 @@ import { IoLogOutOutline } from "react-icons/io5";
 export const LogoutButton = () => {
   return (
     <div className="mt-auto ">
-        <IoLogOutOutline className="w-7 h-7 text-pink-900 hover:text-pink-500"/>
+        <IoLogOutOutline className="w-7 h-7 text-pink-900 hover:text-pink-500 cursor-pointer"/>
     </div>
   )
 }
