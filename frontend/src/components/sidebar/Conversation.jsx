a1085f3a04c30978ffd999ea9c6d79b86fe3b3bc
rebeljelly2147 <c4coder3377@gmail.com>
2025-02-07 07:34:11 +0530

    Implemented real time online status

--- frontend/src/components/sidebar/Conversation.jsx
+++ frontend/src/components/sidebar/Conversation.jsx
@@ -11,8 +11,8 @@ const Conversation = ({ conversation, lastIdx, emoji }) => {
 	return (
 		<>
 			<div
-				className={`flex gap-2 items-center hover:bg-sky-500 rounded p-2 py-1 cursor-pointer
-				${isSelected ? "bg-sky-500" : ""}
+				className={`flex gap-2 items-center hover:bg-pink-950 rounded-lg p-2 py-1 cursor-pointer
+				${isSelected ? "bg-pink-900" : ""}
 			`}
 				onClick={() => setSelectedConversation(conversation)}
 			>
